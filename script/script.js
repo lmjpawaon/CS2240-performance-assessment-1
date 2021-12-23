12381aa0f4d8940dab2d99c21093c7dbcab39ef7
lmjpawaon <lmjpawaon@addu.edu.ph>
2021-12-23 14:38:52 +0800

    initialize script - for navbar transition animation

---
+++ script/script.js
@@ -0,0 +1,9 @@
+//Script for Opening the Navbar
+
+function openNav(){
+    document.getElementById("overlay").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("overlay").style.width = "0%";
+}
